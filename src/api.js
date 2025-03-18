5223aa7ff7420ba39be1490c39efc6a068c73ca7
Josh <josh.duncanson@gmail.com>
2025-03-18 13:15:28 -0400

    Frontend upload to Render

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+fetch(`${API_BASE_URL}/cars`);
 
 export const getCars = async () => {
   const response = await axios.get(`${API_URL}/cars`);
