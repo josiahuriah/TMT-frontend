025c4277c2362f6c9b87ca37fb44744c013ba165
Josh <josh.duncanson@gmail.com>
2025-08-02 11:50:56 -0400

    Removed Error Boundary

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,62 +1,29 @@
+// src/routers/AppRouter.jsx
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer.jsx';
 import NotFoundPage from '../components/NotFoundPage';
 import App from '../App.jsx';
-import ErrorBoundary from '../components/ErrorBoundary';
-import { 
-  AdminPage, 
-  Contact, 
-  AboutUs, 
-  RentalPage,
-  LazyWrapper 
-} from '../components/LazyComponents';
+import RentalPage from '../components/RentalPage';
+import AboutUs from '../components/AboutUs';
+import Contact from '../components/Contact.jsx';
+import AdminPage from '../components/AdminPage.jsx';
 
 const AppRouter = () => (
   <BrowserRouter>
-    <ErrorBoundary>
-      <Header />
-      <div id='body'>
-        <Routes>
-          <Route path='/' element={<App />} />
-          <Route 
-            path='/rental' 
-            element={
-              <LazyWrapper>
-                <RentalPage />
-              </LazyWrapper>
-            } 
-          />
-          <Route 
-            path='/about-us' 
-            element={
-              <LazyWrapper>
-                <AboutUs />
-              </LazyWrapper>
-            } 
-          />
-          <Route 
-            path='/contact' 
-            element={
-              <LazyWrapper>
-                <Contact />
-              </LazyWrapper>
-            } 
-          />
-          <Route 
-            path='/admin/*' 
-            element={
-              <LazyWrapper>
-                <AdminPage />
-              </LazyWrapper>
-            } 
-          />
-          <Route path='/*' element={<NotFoundPage />} />
-        </Routes>
-      </div>
-      <Footer />
-    </ErrorBoundary>
+    <Header />
+    <div id='body'>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/rental' element={<RentalPage />} />
+        <Route path='/about-us' element={<AboutUs />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/admin/*' element={<AdminPage />} />
+        <Route path='/*' element={<NotFoundPage />} />
+      </Routes>
+    </div>
+    <Footer />
   </BrowserRouter>
 );
 
